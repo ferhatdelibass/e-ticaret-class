467600e1341b5d81732443e60fd46d9cf8b64140
Ferhat <ffdelibas@gmail.com>
2023-04-12 23:15:41 +0300

    12.04.2023

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,9 @@
 //// oluşturulan tüm cardlar için kullanılan en dış div ve className belirleme olayını tekrarlamamak adına yazılan bir component
 import React from 'react'
-
-const Card = () => {
+import styles from "./Card.module.scss"
+const Card = ({children, cardClass}) => {
   return (
-    <div>Card</div>
+    <div className={`${styles.card} ${cardClass}`}>{children}</div>
   )
 }
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,25 @@
 //// burada google firebase ile etkileşime buradan girilecek.
+//firebase ile etkileşime geçildi
+import { initializeApp } from "firebase/app";
+//authentication işlemleri için(kullanıcı kaydı)
+import { getAuth } from "firebase/auth";
+//firestore database erişimi için
+import {getFirestore} from "firebase/firestore";
+// storage erişimi için (resim kayıt yeri)
+import { getStorage } from "firebase/storage";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSA1l9QyGUSfMTWuKHVWLf9kXeQIjRctA",
+  authDomain: "e-ticaret-class-19123.firebaseapp.com",
+  projectId: "e-ticaret-class-19123",
+  storageBucket: "e-ticaret-class-19123.appspot.com",
+  messagingSenderId: "108175657057",
+  appId: "1:108175657057:web:6e1bb21a62cd74dfcc0220"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
